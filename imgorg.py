af51b813015bab755ee739418d927728f99f5503
vitaz-0 <vitezslav.zak@seznam.cz>
2018-10-28 21:42:25 +0100

    bla bla

--- imgorg.py
+++ imgorg.py
@@ -50,8 +50,38 @@ def listPhotos():
 def execCommand(command):
     p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     stdout, stderr = p.communicate()
-    print(p.returncode, stdout, stderr)
+    return p.returncode, stdout, stderr
 
+def getFiles(src_dir):
+    files = ls(src_dir)
+    # print(files)
+    return files
+
+def getPhotos():
+    photos=listPhotos()
+    # print(photos)
+    return photos
+
+def process(src_dir, tgt_dir, raw, command):
+    photos = getPhotos()
+    files = getFiles(src_dir)
+    for f in files:
+        fname, fextension = os.path.splitext(f)
+        if len(raw)>0:
+            raw_dir = raw
+        else:
+            raw_dir = src_dir
+
+        if fextension.upper() in ['.JPEG','.PNG']:
+            if (fname+fextension.lower() in photos) or (fname+fextension.upper() in photos):
+                print(f)
+                returncode, stdout, stderr = execCommand(command + [src_dir+'/'+f, tgt_dir])
+
+                rawfile = Path(raw_dir + '/' + fname + ".RW2")
+                # print("RAWFILE:" + str(rawfile))
+                if (rawfile).is_file():
+                    # print("RAW: "+f)
+                    returncode, stdout, stderr = execCommand(command + [rawfile, tgt_dir])
 
 @click.group()
 def imgorg():
@@ -62,53 +92,25 @@ def imgorg():
 @click.argument('tgt_dir', required=True)
 @click.option('-r', '--raw', default='', help='Source folder of RAW files, if different from src_dir')
 def simlink(src_dir, tgt_dir, raw):
-    photos=listPhotos()
-    print(photos)
-    files = ls(src_dir)
-    print(files)
-    for f in files:
-        fname, fextension = os.path.splitext(f)
-        if len(raw)>0:
-            raw_dir = raw
-        else:
-            raw_dir = src_dir
-
-        if fextension.upper() in ['.JPEG','.PNG']:
-            if (fname+fextension.lower() in photos) or (fname+fextension.upper() in photos):
-                print(f)
-                execCommand(['ln', '-s', src_dir+'/'+f, tgt_dir])
-
-                rawfile = Path(raw_dir + '/' + fname + ".RW2")
-                print("RAWFILE:" + str(rawfile))
-                if (rawfile).is_file():
-                    print("RAW: "+f)
-                    execCommand(['ln', '-s', rawfile, tgt_dir])
+    process(src_dir, tgt_dir, raw, ['ln', '-s'])
 
 @imgorg.command()
 @click.argument('src_dir', required=True)
 @click.argument('tgt_dir', required=True)
 @click.option('-r', '--raw', default='', help='Source folder of RAW files, if different from src_dir')
 def mv(src_dir, tgt_dir, raw):
-    photos=listPhotos()
-    files = ls(src_dir)
+    process(src_dir, tgt_dir, raw, ['mv'])
 
-    for f in files:
-        fname, fextension = os.path.splitext(f)
-        if len(raw)>0:
-            raw_dir = raw
-        else:
-            raw_dir = src_dir
+@imgorg.command()
+@click.argument('src_dir', required=True)
+@click.argument('tgt_dir', required=True)
+def cmp(src_dir, tgt_dir):
+    srcFiles = getFiles(src_dir)
+    tgtFiles = getFiles(tgt_dir)
 
-        if fextension.upper() in ['.JPEG','.PNG']:
-            if (fname+fextension.lower() in photos) or (fname+fextension.upper() in photos):
-                print(f)
-                execCommand(['mv', src_dir+'/'+f, tgt_dir])
+    for f in srcFiles:
+        if f in tgtFiles:
+            print("!!! File exists in both folders: " + f)
 
-                rawfile = Path(raw_dir + '/' + fname + ".RW2")
-                print("RAWFILE:" + str(rawfile))
-                if (rawfile).is_file():
-                    print("RAW: "+f)
-                    execCommand(['ln', '-s', rawfile, tgt_dir])
-                    
 if __name__ == '__main__':
     imgorg()
